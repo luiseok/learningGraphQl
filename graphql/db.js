bb1284721791c8f37f1dfbfe30b1221eb7defb59
luiseok <iam@luiseok.com>
2019-02-14 00:35:35 +0900

    Mutation 까지 실습 완료

--- graphql/db.js
+++ graphql/db.js
@@ -54,8 +54,54 @@ export const people = [
         gender: "male"
     },
 ]
+// export const getById = id => {
+//     // const filteredPeople = people.filter(person => id === person.id)
+//     // return filteredPeople[0]
+// }
+let movies = [
+    {
+        id: 0,
+        name: "스타워즈",
+        score: 1
+    },
+    {
+        id: 1,
+        name: "Avengers",
+        score: 8
+    },
+    {
+        id: 2,
+        name: "The Godfather I",
+        score: 99
+    },
+    {
+        id: 3,
+        name: "Logan",
+        score: 22
+    }
+]
+
+export const getMovies = () => movies;
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => id === person.id)
-    return filteredPeople[0]
+    const filteredMovies = movies.filter(m=>m.id === id);
+    return filteredMovies[0];
+}
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(m=>m.id!==id);
+    if(movies.length >  cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true
+    } else return false;
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: movies.length + 1,
+        name,
+        score
+    }
+    movies.push(newMovie)
+    return newMovie
 }
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,19 @@
-import {people, getById} from './db'
+import {getMovies, getById, addMovie, deleteMovie} from './db'
 
 const resolvers = {
     Query: {
-        people: () => people,
-        person: (_, args) => {
-            return getById(args.id)
-        }
+        // people: () => people,
+        // person: (_, args) => {
+        //     return getById(args.id)
+        // }
+        movies : () => getMovies(),
+        movie: (_, {id}) => getById(id)
+    },
+    Mutation: {
+        addMovie: (_, {name, score}) => {
+            return addMovie(name, score)
+        },
+        deleteMovie: (_, {id}) => deleteMovie(id)
     }
 }
 export default resolvers
